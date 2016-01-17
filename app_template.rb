ac96a8ccd4ce069a4aabecffb2c8520c45f549aa
gembaf <gembaf.github@gmail.com>
2016-01-17 09:46:34 +0000

    bootstrap-sassの導入

--- app_template.rb
+++ app_template.rb
@@ -9,7 +9,44 @@ run "echo \"# #{@app_name}\" > README.md"
 # Gemfile
 #=================================================
 
+#-- Bootstrap
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'bootswatch-rails', '>= 3.2'
+
+#-- bundle install
 Bundler.with_clean_env do
   run 'bundle install --path vendor/bundle -j4'
 end
+
+#=================================================
+# app/assets/javascript/application.js
+#=================================================
+
+create_file 'app/assets/javascripts/application.js', <<-JS, force: true
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+JS
+
+#=================================================
+# app/assets/stylesheets/application.sass
+#=================================================
+
+create_file 'app/assets/stylesheets/application.sass', <<-CSS
+/*
+ *= require_tree .
+ *= require_self
+ */
+
+@import "bootstrap-sprockets"
+@import "bootswatch/cerulean/variables"
+@import "bootstrap"
+@import "bootswatch/cerulean/bootswatch"
+@import "overrides"
+CSS
+
+create_file 'app/assets/stylesheets/overrides.sass'
+run 'rm app/assets/stylesheets/application.css'
 
