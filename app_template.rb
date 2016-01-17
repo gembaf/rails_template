35281cbb6223444b8d30d2d09b66344984502fbd
gembaf <gembaf.github@gmail.com>
2016-01-17 07:51:46 +0000

    templateファイルを追加

--- app_template.rb
+++ app_template.rb
@@ -1,4 +1,15 @@
 require 'bundler'
 
-run 'rm README.rdoc'
+@app_name = app_name
+
+run "rm README.rdoc"
+run "echo \"# #{@app_name}\" > README.md"
+
+#=================================================
+# Gemfile
+#=================================================
+
+Bundler.with_clean_env do
+  run 'bundle install --path vendor/bundle -j4'
+end
 
