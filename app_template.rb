2fdadd56c6e353a08c99a272bb96d237a31ee945
gembaf <gembaf.github@gmail.com>
2016-01-18 15:04:16 +0000

    slimを使う設定を追加

--- app_template.rb
+++ app_template.rb
@@ -9,9 +9,17 @@ run "echo \"# #{@app_name}\" > README.md"
 # Gemfile
 #=================================================
 
+#-- Base
+gem 'slim-rails'
+
 #-- Bootstrap
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'bootswatch-rails', '>= 3.2'
+
+#-- development
+gem_group :development do
+  gem 'html2slim'
+end
 
 #-- bundle install
 Bundler.with_clean_env do
@@ -49,4 +57,20 @@ CSS
 
 create_file 'app/assets/stylesheets/overrides.sass'
 run 'rm app/assets/stylesheets/application.css'
+
+#=================================================
+# config/application.rb
+#=================================================
+
+application %q{
+    config.generators do |g|
+      g.template_engine :slim
+    end
+}
+
+#=================================================
+# etc
+#=================================================
+
+run 'bundle exec html2slim -d app/views'
 
